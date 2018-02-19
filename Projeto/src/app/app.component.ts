2b6e7882ba95a109b4f08ea331b5459179a7a86c
Den1lson <denilsondasilvasan@gmail.com>
2018-02-19 17:37:53 -0300

    Classes, atributos e metodos criados em app.component.ts

--- Projeto/src/app/app.component.ts
+++ Projeto/src/app/app.component.ts
@@ -8,3 +8,65 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Listagem de Pessoasng';
 }
+
+export class Usuario{
+
+  nome: string;
+  sobreNome: string;
+  dataNascimento: String;
+  idioma: string;
+  email: string;
+  senha: string;
+  endereco: string;
+  telefone: string;
+
+  amigos:  Usuario[];
+  mesagem: Msm[];
+  novasMensagens: Msm[];
+  notificacoes: string[];
+
+  public set Post(postagem: string){
+
+  }
+
+  public set EnviMensagem(enviar: string){
+
+  }
+
+}
+
+// classe mesnsagem
+
+export class Msm{
+
+  texto: string;
+  //data: date;  não possui o tipo data ????
+  nomeUsr: string;
+
+}
+
+// classe postagens
+
+export class Post{
+
+  texto: string;
+  //data: data;
+  tipoPost: string;
+  nomeUsr: string;
+
+
+}
+
+//classe usuarioService
+
+export class usuarioServ{
+
+  public set cadastrarUrs(cadastrar: string){
+
+  }
+
+  public set login(logar: string){
+
+
+  }
+}
